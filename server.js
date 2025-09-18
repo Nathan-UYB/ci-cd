6703316fd33eed306f918d0948b977bb0e481eaf
Nathan-UYB <nathanboudon99@gmail.com>
2025-09-18 15:41:23 +0200

    Update de la pipline

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const client = require("prom-client");
 
 const app = express();
-const port = 3000;
+const port = 3001;
 
 // ----- Prometheus metrics -----
 const collectDefaultMetrics = client.collectDefaultMetrics;
